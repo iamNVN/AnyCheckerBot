568f7ae5c68163e81712817f32c55a0d7f03ae8f
Ninja Naveen <67575446+ninjanaveen@users.noreply.github.com>
2020-12-06 18:26:06 +0530

    Update proxy.php

--- Plugins/proxy.php
+++ Plugins/proxy.php
@@ -7,7 +7,7 @@ if (strpos($text, "/http") === 0){
             $amount = file_get_contents("https://api.proxyscrape.com?request=amountproxies&proxytype=http&timeout=5000&country=all&ssl=all&anonymity=all");
             $last_updated = file_get_contents('https://api.proxyscrape.com?request=lastupdated&proxytype=http');
             $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot1382500231:AAEj9CBl3lgHtM9DkA6er_mwoP3Gxv56sd8/sendDocument');
+      curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot'.$API_KEY.'/sendDocument');
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_POST, 1);
       $post = array(
@@ -30,7 +30,7 @@ if (strpos($text, "/socks4") === 0){
             $amount = file_get_contents("https://api.proxyscrape.com?request=amountproxies&proxytype=socks4&timeout=5000&country=all");
             $last_updated = file_get_contents('https://api.proxyscrape.com?request=lastupdated&proxytype=socks4');
             $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot1368148516:AAH45_hG0-p9PtIS1kNfRrUBph2CYYAKfIU/sendDocument');
+      curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot'.$API_KEY.'/sendDocument');
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_POST, 1);
       $post = array(
@@ -53,7 +53,7 @@ if (strpos($text, "/socks5") === 0){
             $amount = file_get_contents("https://api.proxyscrape.com?request=amountproxies&proxytype=socks5&timeout=5000&country=all");
             $last_updated = file_get_contents('https://api.proxyscrape.com?request=lastupdated&proxytype=socks5');
             $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot1368148516:AAH45_hG0-p9PtIS1kNfRrUBph2CYYAKfIU/sendDocument');
+      curl_setopt($ch, CURLOPT_URL, 'https://api.telegram.org/bot'.$API_KEY.'/sendDocument');
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_POST, 1);
       $post = array(
@@ -67,11 +67,5 @@ if (strpos($text, "/socks5") === 0){
 
       curl_exec($ch);
           }
-
-
-/////////////////////==========[PROXY]==========////////////////
-          
-if ($text == '/proxy') {
-            sendMessage($chat_id, "<b>Available Commands%0A/proxy http - Sends Fresh </b><strong>HTTP Proxies</strong><b>%0A/proxy socks4 - Sends Fresh </b><strong>Socks4 Proxies</strong><b>%0A/proxy socks5 - Sends Fresh </b><strong>Socks5 Proxies</strong><b>%0A%0A💠 I'm made by @IndianBots</b>", $message_id);}
 
 ?>
