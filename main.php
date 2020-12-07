9fabe16c8a1d9f544e08e512c935d23cf75bbf2d
Ninja Naveen <67575446+ninjanaveen@users.noreply.github.com>
2020-12-07 11:15:59 +0530

    Update main.php

--- main.php
+++ main.php
@@ -18,6 +18,7 @@ include "./Plugins/weather.php";
 include "./Plugins/dictionary.php";
 include "./Plugins/proxy.php";
 include "./Plugins/covid.php";
+include "./Plugins/currency.php";
 
 
 ////////////////=========[START MESSAGE]=========////////////////
@@ -70,7 +71,7 @@ I can do Several Things!</b>
 Click on the buttons below to get documentation about specific modules!",
 	'parse_mode'=>'html',
 	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Bin Checker",'callback_data'=>"bin"],['text'=>"Dictionary",'callback_data'=>"dict"],['text'=>"IBAN Checker",'callback_data'=>"iban"]],[['text'=>"Proxy Scrapper",'callback_data'=>"proxy"],['text'=>"SK Checker",'callback_data'=>"stripe"],['text'=>"Weather",'callback_data'=>"weather"]],
+	[['text'=>"Bin Checker",'callback_data'=>"bin"],['text'=>"Covid-19 Stats",'callback_data'=>"covid"],['text'=>"Currency Converter",'callback_data'=>"monigay"]['text'=>"Dictionary",'callback_data'=>"dict"],['text'=>"IBAN Checker",'callback_data'=>"iban"]],[['text'=>"Proxy Scrapper",'callback_data'=>"proxy"],['text'=>"SK Checker",'callback_data'=>"stripe"],['text'=>"Weather",'callback_data'=>"weather"]],
 	],'resize_keyboard'=>true])
 	]);
 }
@@ -88,7 +89,7 @@ Click on the buttons below to get documentation about specific modules!",
 	'parse_mode'=>'html',
 	'reply_to_message_id'=> $message_id,
 	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Bin Checker",'callback_data'=>"bin"],['text'=>"Dictionary",'callback_data'=>"dict"],['text'=>"IBAN Checker",'callback_data'=>"iban"]],[['text'=>"Proxy Scrapper",'callback_data'=>"proxy"],['text'=>"SK Checker",'callback_data'=>"stripe"],['text'=>"Weather",'callback_data'=>"weather"]],
+	[['text'=>"Bin Checker",'callback_data'=>"bin"],['text'=>"Covid-19 Stats",'callback_data'=>"covid"],['text'=>"Currency Converter",'callback_data'=>"monigay"]['text'=>"Dictionary",'callback_data'=>"dict"],['text'=>"IBAN Checker",'callback_data'=>"iban"]],[['text'=>"Proxy Scrapper",'callback_data'=>"proxy"],['text'=>"SK Checker",'callback_data'=>"stripe"],['text'=>"Weather",'callback_data'=>"weather"]],
 	],'resize_keyboard'=>true])
 	]);
 }
@@ -105,6 +106,47 @@ if($data == "bin"){
 Command:</b>
 
 /bin <code>&lt;bin&gt;</code> - Checks the Bin and Provides Information",
+	'parse_mode'=>'html',
+	'reply_markup'=>json_encode(['inline_keyboard'=>[
+	[['text'=>"Return",'callback_data'=>"help"]],
+	],'resize_keyboard'=>true])
+	]);
+}
+
+
+////////////////=========[COVID-19 STATS]=========////////////////
+
+if($data == "covid"){
+	bot('editMessageText',[
+	'chat_id'=>$chatid,
+	'message_id'=>$messageid,
+	'text'=>"<b>🌀 Covid-19 Stats 🌀
+
+Command:</b>
+
+/covid - Provides Worldwide Covid-19 Stats
+/covid <code>&lt;country&gt;</code> - Provides Covid-19 Stats for the Given Country",
+	'parse_mode'=>'html',
+	'reply_markup'=>json_encode(['inline_keyboard'=>[
+	[['text'=>"Return",'callback_data'=>"help"]],
+	],'resize_keyboard'=>true])
+	]);
+}
+
+
+////////////////=========[CURRENCY CONVERTER]=========////////////////
+
+if($data == "monigay"){
+	bot('editMessageText',[
+	'chat_id'=>$chatid,
+	'message_id'=>$messageid,
+	'text'=>"<b>🌀 Currency Converter 🌀
+
+Command:</b>
+
+/cash <code>&lt;Value&gt;&lt;from&gt;&lt;to&gt;</code> - Converts the Currency
+
+<b>Example:- /cash 20 USD INR - Converts 20 USD to INR</b>",
 	'parse_mode'=>'html',
 	'reply_markup'=>json_encode(['inline_keyboard'=>[
 	[['text'=>"Return",'callback_data'=>"help"]],
