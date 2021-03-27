16e5459bc592b2761a926a27fa6fb296b854bf31
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-03-27 15:28:44 +0530

    main.php

--- main.php
+++ main.php
@@ -34,7 +34,7 @@ Click the Button Below to open help menu!</b>",
 	'parse_mode'=>'html',
 	'reply_to_message_id'=> $message_id,
 	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Help 💬",'callback_data'=>"help"]],[['text'=>"Our Channel 🌐",'url'=>"https://t.me/IndianBots"],['text'=>"My Source Code ✅",'url'=>"https://github.com/IndianBots/AnyCheckerBot"]]
+	[['text'=>"Help 💬",'callback_data'=>"help"]],[['text'=>"Our Channel 🌐",'url'=>"https://t.me/MadPremium"]
   ],'resize_keyboard'=>true])
 	
   ]);
